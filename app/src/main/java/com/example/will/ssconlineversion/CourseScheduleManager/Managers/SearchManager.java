d8d0d90b35a7fc5afb661f859bdd7a9b8949d268
WillOZhang <zz051866@>
2017-07-30 14:05:45 -0700

    ...

--- app/src/main/java/com/example/will/ssconlineversion/CourseScheduleManager/Managers/SearchManager.java
+++ app/src/main/java/com/example/will/ssconlineversion/CourseScheduleManager/Managers/SearchManager.java
@@ -1,9 +1,29 @@
 package com.example.will.ssconlineversion.CourseScheduleManager.Managers;
 
+import com.example.will.ssconlineversion.CourseScheduleManager.Building;
+import com.example.will.ssconlineversion.CourseScheduleManager.Item;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Will on 2017/7/29.
  */
 
 public class SearchManager {
+    private static SearchManager instance;
+    private Set<Item> items;
+
+    private SearchManager() {
+        this.items = new HashSet<Item>();
+    }
+
+    public static SearchManager getInstance() {
+        // Do not modify the implementation of this method!
+        if(instance == null) {
+            instance = new SearchManager();
+        }
+        return instance;
+    }
 
 }
